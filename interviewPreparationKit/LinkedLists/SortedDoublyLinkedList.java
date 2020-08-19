a42a9805036b0ba54cc146e514527cdaf3721d4c
Divya <divyass136@gmail.com>
2020-08-19 23:15:23 +0530

    refs #HackerRankLinkedLists

---
+++ interviewPreparationKit/LinkedLists/SortedDoublyLinkedList.java
@@ -0,0 +1,136 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SortedDoublyLinkedList {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the sortedInsert function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode head, int data) {
+         DoublyLinkedListNode ptr = head;
+        while(ptr.data<data && ptr.next != null){
+            ptr=ptr.next;
+        }
+        DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
+        if(ptr.prev != null && ptr.next != null) {
+            ptr.prev.next = newNode;
+            newNode.prev = ptr.prev;
+            ptr.prev = newNode;
+            newNode.next = ptr;
+        }else if(ptr.next == null ){
+            if(ptr.data<data) {
+                ptr.next = newNode;
+                newNode.prev = ptr;
+                newNode.next = null;
+            }else {
+                newNode.next = ptr;
+                newNode.prev = ptr.prev;
+                ptr.prev.next= newNode;
+            }
+        }else if(ptr.prev == null) {
+            newNode.next = ptr;
+            ptr.prev= newNode;
+            newNode.prev=null;
+            head = newNode;
+        }
+
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            int data = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            DoublyLinkedListNode llist1 = sortedInsert(llist.head, data);
+
+            printDoublyLinkedList(llist1, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
